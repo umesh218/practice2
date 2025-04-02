4eaee937fe6bc97a882239c5fad3b3076b03f52b
umesh218 <umesh16517@gmail.com>
2025-04-02 11:03:26 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,8 @@ variable "vm_name" { default = "myVM" }
 variable "admin_username" { default = "azureuser" }
 variable "vm_size" { default = "Standard_B1s" }
 variable "ssh_key" { default = "~/.ssh/id_rsa.pub" }
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
